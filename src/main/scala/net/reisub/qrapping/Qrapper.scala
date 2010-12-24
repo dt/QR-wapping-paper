c8bedd72eba63c7ca2d0061df5c5baed04e521a7
David Taylor <dltaylor@andrew.cmu.edu>
2010-12-23 19:16:58 -0800

    Use the ShuffleBag to allocate sizes

--- src/main/scala/net/reisub/qrapping/Qrapper.scala
+++ src/main/scala/net/reisub/qrapping/Qrapper.scala
@@ -7,10 +7,14 @@ object Qrapper {
 	val things = List("raa raa ah ah ah", "ro ma ro mama", "ga ga", "ooh la la")
 
 	val canvas = new Canvas( 8.5, 11, 300)
+
+	val sizes = new ShuffleBag[Int]();
+	sizes.addMany( (300 * 2.5).toInt, 2);
+	sizes.addMany( (300 * 1.5).toInt, 5);
+	sizes.addMany( (300 * 0.75).toInt, 8);
 	
-	val size = 400
 	
-	val items = things.map( content => new Item(size, content))
+	val items = things.map( content => new Item(sizes.draw(), content))
 
 	val placement = new GridPlacement(canvas, items)
 	
