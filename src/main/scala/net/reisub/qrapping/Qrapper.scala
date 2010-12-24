aab6d9e4733ad6b488d3ff1e975b73ae4fdf91ea
David Taylor <dltaylor@andrew.cmu.edu>
2010-12-23 16:52:43 -0800

    First pass at functionality

--- src/main/scala/net/reisub/qrapping/Qrapper.scala
+++ src/main/scala/net/reisub/qrapping/Qrapper.scala
@@ -4,14 +4,42 @@ import com.google.zxing._
 import qrcode._
 import client.j2se._
 import java.io.File
+import java.awt.Image
+import java.awt.image.ImageObserver
 import java.awt.image.BufferedImage
-import java.Graphics
 
-object Qrapper {
+object Qrapper extends ImageObserver {
   def main(args : Array[String]) : Unit = {
-	  println("Hello World")
-	  
-	  val writer = new QRCodeWriter();
-	  writer.encode()
+	val things = List("raa raa ah ah ah", "ro ma ro mama", "ga ga", "ooh la la")
+
+		
+	val width : Double = 8
+	val height : Double = 10.5
+	val dpi : Int = 300
+	
+	val x_dots = (dpi.toFloat * width) toInt
+	val y_dots = (dpi.toFloat * height) toInt
+	
+	val canvas = new BufferedImage(x_dots, y_dots, BufferedImage.TYPE_INT_ARGB);
+	
+	val size = 100
+	val writer = new QRCodeWriter
+
+	val qrcodes = things.map(s => 
+	  {
+		val matrix = writer encode(s, BarcodeFormat.QR_CODE, size, size, null)
+		MatrixToImageWriter toBufferedImage(matrix)
+	  }
+	)
+	
+	val g = canvas getGraphics
+	
+	for(qrcode <- qrcodes)
+		g.drawImage(qrcode, 0, 0, this)
+	println("Done")
+  }
+  
+  def imageUpdate(img:Image, infoflags : Int, x :Int, y:Int, width:Int, height:Int ) : Boolean = {
+   false
   }
 }
