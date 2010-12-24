2d8141930fe54a2e4cd7408f3f58a47bd91741f3
David Taylor <dltaylor@andrew.cmu.edu>
2010-12-23 17:12:47 -0800

    tiling canvas version

--- src/main/scala/net/reisub/qrapping/Qrapper.scala
+++ src/main/scala/net/reisub/qrapping/Qrapper.scala
@@ -4,6 +4,8 @@ import com.google.zxing._
 import qrcode._
 import client.j2se._
 import java.io.File
+import javax.imageio.ImageIO;
+
 import java.awt.Image
 import java.awt.image.ImageObserver
 import java.awt.image.BufferedImage
@@ -16,13 +18,13 @@ object Qrapper extends ImageObserver {
 	val width : Double = 8
 	val height : Double = 10.5
 	val dpi : Int = 300
+	val size = 400
 	
 	val x_dots = (dpi.toFloat * width) toInt
 	val y_dots = (dpi.toFloat * height) toInt
 	
 	val canvas = new BufferedImage(x_dots, y_dots, BufferedImage.TYPE_INT_ARGB);
 	
-	val size = 100
 	val writer = new QRCodeWriter
 
 	val qrcodes = things.map(s => 
@@ -34,8 +36,21 @@ object Qrapper extends ImageObserver {
 	
 	val g = canvas getGraphics
 	
-	for(qrcode <- qrcodes)
-		g.drawImage(qrcode, 0, 0, this)
+	var x_pos = 0;
+	var y_pos = 0;
+	
+	for(qrcode <- qrcodes) {
+		x_pos = x_pos + size
+		
+		if(x_pos > x_dots) {
+			x_pos = 0
+			y_pos = y_pos + size
+		}
+			
+		g.drawImage(qrcode, x_pos, y_pos, this)
+	}
+	
+	ImageIO.write(canvas, "png", new File("test.png"))
 	println("Done")
   }
   
