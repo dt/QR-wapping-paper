f5ddc61372d6ce6395eae996d2b48a49e32fe730
Matthew Hooker <mhooker@digg.com>
2010-12-23 22:24:31 -0500

    adjust image size

--- src/main/scala/net/reisub/qrapping/Placement.scala
+++ src/main/scala/net/reisub/qrapping/Placement.scala
@@ -1,15 +1,16 @@
 package net.reisub.qrapping
 import scala.util.Random
+import scala.math
 
 /* case class OutOfRoomException extends Exception */
 
 
 /*
 abstract class Placement(canvas : Canvas) {
-  var items = List()
+  var qrcodes = List()
 
-  def addQRCode(qrcode : Item) : Unit = {
-    items = items :: qrcode
+  def addQRCode(qrcode : qrcode) : Unit = {
+    qrcodes = qrcodes :: qrcode
   }
   
 
@@ -17,37 +18,38 @@ abstract class Placement(canvas : Canvas) {
 
 */
 
-class GridPlacement(canvas :Canvas, items : List[Item] ) /* extends Placement*/ {
+class GridPlacement(canvas :Canvas, qrcodes : List[qrcode] ) /* extends Placement*/ {
 	
 	def calculate() = {
 		
       val g = canvas.buffer getGraphics
 
       //A cell is the smallest unit on the grid. unit in pixels
-      val smallest_item = items min Ordering.by((_:Item).size)
-      val cell_size = smallest_item.size
+      val smallest_qrcode = qrcodes min Ordering.by((_:qrcode).size)
+      //make sure the cell size is the diagonal of the smalled "qrcode"
+      val cell_size = math.sqrt(2 * math.pow(smallest_qrcode.size,2)) toInt
       
           
       val cells_x = canvas.x_dots / cell_size
       val cells_y = canvas.y_dots / cell_size
-      for (item <- items) {
+      for (qrcode <- qrcodes) {
         val rand_x = Random.nextInt(cells_x)
         val rand_y = Random.nextInt(cells_y)
-        canvas.add(item.getImage, rand_x * cell_size, rand_y * cell_size)
+        canvas.add(qrcode.getImage, rand_x * cell_size, rand_y * cell_size)
       }
       /*
       var x_pos = 0
       var y_pos = 0
       
-      for(item <- items) {
-          x_pos = x_pos + item.size
+      for(qrcode <- qrcodes) {
+          x_pos = x_pos + qrcode.size
           
           if(x_pos > canvas.x_dots) {
               x_pos = 0
-              y_pos = y_pos + item.size
+              y_pos = y_pos + qrcode.size
           }
               
-          canvas.add(item.getImage, x_pos, y_pos)
+          canvas.add(qrcode.getImage, x_pos, y_pos)
       }
       */
 	}
