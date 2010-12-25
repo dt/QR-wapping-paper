ebf54c663a9d19621644aa4dee7b7e2e75bbf845
David Taylor <dltaylor@andrew.cmu.edu>
2010-12-24 19:46:43 -0800

    Version bump

--- src/main/scala/net/reisub/qrapping/Canvas.scala
+++ src/main/scala/net/reisub/qrapping/Canvas.scala
@@ -8,21 +8,20 @@ import java.awt.Color
 
 
 class Canvas( paperSize : (Int,Int) ) extends ImageObserver {
-	val (width, height) = paperSize
+  val (width, height) = paperSize
 	
-	val buffer = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB)
-	val graphics = buffer createGraphics()
-	graphics setPaint ( Color.WHITE );
-	graphics fillRect ( 0, 0, width, height );
+  val buffer = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB)
+  val graphics = buffer createGraphics()
+  graphics setPaint ( Color.WHITE );
+  graphics fillRect ( 0, 0, width, height );
 
-	def add(item:Item, x_pos : Int, y_pos : Int) = {
-		buffer getGraphics() drawImage(item.image, x_pos, y_pos, this)
-	}
+  def add(item:Item, x_pos : Int, y_pos : Int) = {
+    buffer getGraphics() drawImage(item.image, x_pos, y_pos, this)
+  }
 	
-	def write(format: String,  filename: String) = {
-			ImageIO.write(buffer, "png", new File(filename))
-	}
-
+  def write(format: String,  filename: String) = {
+    ImageIO.write(buffer, "png", new File(filename))
+  }
 
   def imageUpdate(img:Image, infoflags : Int, x :Int, y:Int, width:Int, height:Int ) : Boolean = {
    false
